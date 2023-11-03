e52c2e23cf992f25012c74d2dc8c571fbeaab8fd
tsubasaogawa <hurikake@gmail.com>
2023-11-03 23:48:06 +0900

    test: add simple tests

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,15 +1,19 @@
 const assert = require('assert');
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 const vscode = require('vscode');
-// const myExtension = require('../extension');
+const opener = require('../../extension');
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('getProvider', () => {
+		assert.strictEqual('', opener.getProvider(''));
+		assert.strictEqual('', opener.getProvider('foo'));
+		assert.strictEqual('foo', opener.getProvider('foo_resource'));
 	});
+
+	test('generateUrl', () => {
+		assert.strictEqual('', opener.generateUrl('foo_resource', 'foo'));
+		assert.notStrictEqual('', opener.generateUrl('aws_resource', 'aws'));
+	})
 });
